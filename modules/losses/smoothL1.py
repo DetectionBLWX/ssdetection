2cf5d0eb9a55a9481e10f59d6f15ab192798b986
CharlesPikachu <1159254961@qq.com>
2019-11-26 19:31:28 +0800

    Update smoothL1.py

--- modules/losses/smoothL1.py
+++ modules/losses/smoothL1.py
@@ -3,9 +3,9 @@ import torch
 
 
 '''smooth l1 loss'''
-def smoothL1Loss(bbox_pred, bbox_targets, bbox_inside_weights, bbox_outside_weights, sigma=1.0, dim=[1]):
+def smoothL1Loss(bbox_preds, bbox_targets, bbox_inside_weights, bbox_outside_weights, sigma=1.0, dim=[1]):
 	sigma_2 = sigma ** 2
-	box_diff = bbox_pred - bbox_targets
+	box_diff = bbox_preds - bbox_targets
 	in_box_diff = bbox_inside_weights * box_diff
 	abs_in_box_diff = torch.abs(in_box_diff)
 	smoothL1_sign = (abs_in_box_diff < 1. / sigma_2).detach().float()
@@ -19,8 +19,8 @@ def smoothL1Loss(bbox_pred, bbox_targets, bbox_inside_weights, bbox_outside_weig
 
 
 '''smooth l1 loss with beta'''
-def betaSmoothL1Loss(bbox_pred, bbox_targets, beta=1, size_average=True):
-	diff = torch.abs(bbox_pred - bbox_targets)
+def betaSmoothL1Loss(bbox_preds, bbox_targets, beta=1, size_average=True):
+	diff = torch.abs(bbox_preds - bbox_targets)
 	loss = torch.where(diff < beta, 0.5 * diff ** 2 / beta, diff - 0.5 * beta)
 	loss = loss.mean() if size_average else loss.sum()
 	return loss
