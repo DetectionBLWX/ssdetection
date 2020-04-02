c9e86ac20846711e5253a2a736fb72265f25b7f4
CharlesPikachu <1159254961@qq.com>
2020-04-02 17:12:59 +0800

    implement some losses

---
+++ modules/losses/IoULoss.py
@@ -0,0 +1,33 @@
+'''
+Function:
+	define the iou losses
+Author:
+	Charles
+'''
+import torch
+
+
+'''giou loss, I borrow the code from mmdet'''
+def GIoULoss(bbox_preds, bbox_targets, eps=1e-7, size_average=True, loss_weight=1.0):
+	# overlap
+	lt = torch.max(bbox_preds[:, :2], bbox_targets[:, :2])
+	rb = torch.min(bbox_preds[:, 2:], bbox_targets[:, 2:])
+	wh = (rb - lt + 1).clamp(min=0)
+	overlap = wh[:, 0] * wh[:, 1]
+	# union
+	ap = (bbox_preds[:, 2] - bbox_preds[:, 0] + 1) * (bbox_preds[:, 3] - bbox_preds[:, 1] + 1)
+	ag = (bbox_targets[:, 2] - bbox_targets[:, 0] + 1) * (bbox_targets[:, 3] - bbox_targets[:, 1] + 1)
+	union = ap + ag - overlap + eps
+	# IoU
+	ious = overlap / union
+	# enclose area
+	enclose_x1y1 = torch.min(bbox_preds[:, :2], bbox_targets[:, :2])
+	enclose_x2y2 = torch.max(bbox_preds[:, 2:], bbox_targets[:, 2:])
+	enclose_wh = (enclose_x2y2 - enclose_x1y1 + 1).clamp(min=0)
+	enclose_area = enclose_wh[:, 0] * enclose_wh[:, 1] + eps
+	# GIoU
+	gious = ious - (enclose_area - union) / enclose_area
+	loss = 1 - gious
+	# summary and return the loss
+	loss = loss.mean() if size_average else loss.sum()
+	return loss * loss_weight
--- modules/losses/smoothL1.py
+++ modules/losses/smoothL1.py
@@ -1,10 +1,26 @@
-'''define the smooth l1 loss'''
+'''
+Function:
+	define the smooth l1 losses
+Author:
+	Charles
+'''
 import torch
+import numpy as np
 
 
 '''smooth l1 loss with beta'''
 def betaSmoothL1Loss(bbox_preds, bbox_targets, beta=1, size_average=True, loss_weight=1.0):
 	diff = torch.abs(bbox_preds - bbox_targets)
 	loss = torch.where(diff < beta, 0.5 * diff ** 2 / beta, diff - 0.5 * beta)
+	loss = loss.mean() if size_average else loss.sum()
+	return loss * loss_weight
+
+
+'''balanced smooth l1 Loss, I borrow the code from mmdet'''
+def balancedSmoothL1Loss(bbox_preds, bbox_targets, beta=1.0, alpha=0.5, gamma=1.5, size_average=True, loss_weight=1.0):
+	assert (beta > 0.) and (bbox_preds.size() == bbox_targets.size()) and (bbox_targets.numel() > 0)
+	diff = torch.abs(bbox_preds - bbox_targets)
+	b = np.e ** (gamma / alpha) - 1
+	loss = torch.where(diff < beta, alpha / b * (b * diff + 1) * torch.log(b * diff / beta + 1) - alpha * diff, gamma * diff + gamma / b - alpha * beta)
 	loss = loss.mean() if size_average else loss.sum()
 	return loss * loss_weight
